3386f7af9b66ee6b8ee524b094278540ec90f99a
yashLGN <lgnsound77@gmail.com>
2025-07-12 10:03:11 +0530

    Basic server running , empty html connected , basic setup.

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5001)
